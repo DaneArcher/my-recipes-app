cc1cf9a7d14d2735198d538244c52a338c0d06b3
DaneArcher <danearcher1@gmail.com>
2020-07-16 13:43:48 -0700

    added componets file and minor changes to App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import AddRecipe from './components/Add_recipe'
 
 function App() {
   return (
     <div className="App">
+      <AddRecipe />
     </div>
   );
 }
---
+++ my-react-app/src/components/Add_recipe.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class AddRecipe extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Testing</h1>
+            </div>
+        )
+    }
+
+}
+
+export default AddRecipe;
