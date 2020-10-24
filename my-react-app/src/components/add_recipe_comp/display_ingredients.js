9b649b621a3dda7cefba3d690c156323fabe7a3b
DaneArcher <danearcher1@gmail.com>
2020-10-23 19:43:31 -0700

    changed display recipe file to display ingredents

---
+++ my-react-app/src/components/add_recipe_comp/display_ingredients.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Button, Segment, Icon} from 'semantic-ui-react';
+
+
+const DisplayIngredients = ({ingredients, deletIngredient}) =>{
+    const ingredientsList = ingredients.length ? (
+        ingredients.map((ingredient,index) => {
+            return(
+                <div key={ingredient}>
+                    {/*
+                    <p>{ingredient} <button onClick={()=>{deletIngredient(index)}}>X</button></p>
+                   
+                    */} 
+                    <Segment>
+                        {ingredient}
+                        <Button 
+                            floated='right' 
+                            icon
+                            onClick={()=>{deletIngredient(index)}} 
+                            color='red' 
+                            size='mini'>
+                            <Icon name='close'/>
+                        </Button>
+                    </Segment>
+                    {/**/}
+                </div>
+            )
+        })
+    ):(
+        <p>You have no ingredients added yet.</p>
+    )
+
+    return(
+        <div>
+            {ingredientsList}
+        </div>
+    )
+
+}
+export default DisplayIngredients
