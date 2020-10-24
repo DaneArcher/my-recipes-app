0b0738d4bda119288d9898876db9e64b633b22c3
DaneArcher <danearcher1@gmail.com>
2020-10-23 19:48:12 -0700

    now uses react router dom to switch between add and search recipes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,38 +1,26 @@
-import React, {useEffect}from 'react';
+import React from 'react';
 import AddRecipe from './components/Add_recipe'
 import SearchRecipe from './components/Search_recipe'
-
+import Navbar from './components/NavBar'
+import DisplayRecipe from './components/DisplayRecipe'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 
 function App() {
-
-/*
-  useEffect(() => {
-    fetch("/scrape_recipes?link=https://www.foodnetwork.com/recipes/food-network-kitchen/prosciutto-wrapped-chicken-kebabs-3362756")
-    .then(response => response.json())
-    .then(data => {console.log(data)})
-  },[]);
-  
- useEffect(() =>{
-    const data = { username: 'example' };
-
-    fetch('/add_recipes', {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    }).then(response => response.text())
-    .then(text => {console.log(text)})
- },[])*/
-
-
-
+  return(
+    <DisplayRecipe/>
+  )
+  /*
   return (
-    <div className="App">
-      <SearchRecipe/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Route exact path='/' component={SearchRecipe}/>
+        <Route path='/add' component={AddRecipe}/>
+      </div>
+    </BrowserRouter>
   );
+  */
 }
 
 export default App;
