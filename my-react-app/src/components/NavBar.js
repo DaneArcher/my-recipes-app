67a9b0cae1178831c86d3cd0c2584cd4a89c6d7f
DaneArcher <danearcher1@gmail.com>
2020-10-23 19:40:37 -0700

    created a NavBar component

---
+++ my-react-app/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Menu} from 'semantic-ui-react';
+import {Link} from 'react-router-dom'
+
+
+const Navbar = () =>{
+	return(
+		<Menu>
+			<Menu.Menu  position='right'>
+				<Menu.Item>
+					<Link to="/">Search</Link>
+				</Menu.Item>
+				<Menu.Item>
+					<Link to="/add">Add</Link>
+				</Menu.Item>
+			</Menu.Menu>
+		</Menu>
+	)
+}
+export default Navbar;
