78fa96328ff180a31051bc3fd44b78c987889eb2
Sanjay G <sanjay.garimella@gmail.com>
2020-11-05 13:26:26 -0800

    Uncommented fetch code in component did mount. Made other minor changes to get code working

--- my-react-app/src/components/DisplayRecipe.js
+++ my-react-app/src/components/DisplayRecipe.js
@@ -13,27 +13,27 @@ class DisplayRecipe extends Component{
     }
     componentDidMount(){
         console.log('component did mount')
-        let recipe = {
-            title: 'Sun-Dried Romatoes',
-            author: 'Cuoredicioccolato',
-            src: 'YouTube',
-            quick_description: 'Tasty home made Italian sun dried tomatoes!',
-            total_time: '7 Days',
-            prep_time: '30 minutes',
-            cook_time: '7 Days',
-            rating: '4/5',
-            serving: '25',
-            calories: '100',
-            ingredients: ['25 Roma Tomatoes','1 Quarte Olive Oil','1 Clove of Garlic','2 tablespoons of Spices'],
-            ingredient_tags: ['Roma Tomatoes','Olive Oil', 'Garlic','Spices'],
-            directions: 'sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
-            chef_notes: 'you can also do them in an oven at 125 f',
-            url: 'https://www.youtube.com/watch?v=5yTWGjPdj-g'
-        }
-        this.setState({recipe})
-        /*
-        console.log(this.props.recipe_id)
-        let link = '/get_recipe_by_id?id='
+        // let recipe = {
+        //     title: 'Sun-Dried Romatoes',
+        //     author: 'Cuoredicioccolato',
+        //     src: 'YouTube',
+        //     quick_description: 'Tasty home made Italian sun dried tomatoes!',
+        //     total_time: '7 Days',
+        //     prep_time: '30 minutes',
+        //     cook_time: '7 Days',
+        //     rating: '4/5',
+        //     servings: '25',
+        //     calories: '100',
+        //     ingredients: ['25 Roma Tomatoes','1 Quarte Olive Oil','1 Clove of Garlic','2 tablespoons of Spices'],
+        //     ingredient_tags: ['Roma Tomatoes','Olive Oil', 'Garlic','Spices'],
+        //     directions: 'sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
+        //     chef_notes: 'you can also do them in an oven at 125 f',
+        //     url: 'https://www.youtube.com/watch?v=5yTWGjPdj-g'
+        // }
+        // this.setState({recipe})
+        
+        //console.log(this.props.recipe_id)
+        let link = '/full_recipe?recipe_id='
         let recipe_id = this.props.recipe_id        
         link = link.concat(recipe_id)
 
@@ -41,10 +41,11 @@ class DisplayRecipe extends Component{
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            console.log(data.recipe)
             this.setState({
                 recipe: data.recipe
             })
-        */
+        })
     }
     check = () =>{
         let packaged_data = []
@@ -73,8 +74,8 @@ class DisplayRecipe extends Component{
         if (recipe['rating'] !== ''){
             descriptors_list = [...descriptors_list, <Segment compact>rating: {recipe['rating']}</Segment>]
         }
-        if (recipe['serving'] !== ''){
-            descriptors_list = [...descriptors_list, <Segment compact>Serving: {recipe['serving']}</Segment>]
+        if (recipe['servings'] !== ''){
+            descriptors_list = [...descriptors_list, <Segment compact>Serving: {recipe['servings']}</Segment>]
         }
         if (recipe['calories'] !== ''){
             descriptors_list = [...descriptors_list, <Segment compact>Calories Per Serving: {recipe['calories']}</Segment>]
@@ -115,8 +116,10 @@ class DisplayRecipe extends Component{
         packaged_data.push(url)
         return(packaged_data)
     }
-   render(){
-       console.log('in render')
+   render(){   
+        console.log('in render')
+    //    console.log(this.props.recipe_id)
+    //    console.log(this.props)
        let {recipe} = this.state
        if(recipe != null){
             let packaged_data = this.check()
