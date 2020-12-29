5532ba1030fcd513fa9b0385b408ca90291f8f46
DaneArcher <danearcher1@gmail.com>
2020-12-29 10:47:48 -0800

    moved DisplayRecipe into DisplayRecipe Folder and rewrote code getting rid of semantic ui

---
+++ my-react-app/src/components/DisplayRecipe_Folder/DisplayRecipe.js
@@ -0,0 +1,137 @@
+import React, {Component} from 'react'
+
+class DisplayRecipe extends Component{
+    state = {
+        recipe: null
+    }
+    componentDidMount(){
+        console.log('component did mount')
+        let recipe = {
+            title: 'Sun-Dried Romatoes',
+            author: 'Cuoredicioccolato',
+            src: 'YouTube',
+            quick_description: 'Tasty home made Italian sun dried tomatoes!',
+            total_time: '7 Days',
+            prep_time: '30 minutes',
+            cook_time: '7 Days',
+            rating: '4/5',
+            servings: '25',
+            calories: '100',
+            ingredients: ['25 Roma Tomatoes','1 Quarte Olive Oil','1 Clove of Garlic','2 tablespoons of Spices'],
+            ingredient_tags: ['Roma Tomatoes','Olive Oil', 'Garlic','Spices'],
+            directions: 'sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+            chef_notes: 'you can also do them in an oven at 125 f',
+            url: 'https://www.youtube.com/watch?v=5yTWGjPdj-g',
+            img: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/10/14/0/FNK_SLOW_COOKER_HOT_CHOCOLATE_H_f_s4x3.jpg.rend.hgtvcom.826.620.suffix/1602696813966.jpeg'
+        }
+        this.setState({recipe})
+        /*
+        let link = 'http://localhost:5000/full_recipe?recipe_id='
+        let recipe_id = this.props.recipe_id        
+        link = link.concat(recipe_id)
+
+        fetch(link)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            console.log(data.recipe)
+            this.setState({
+                recipe: data.recipe
+            })
+        })*/        
+    }
+    checkRecipe = () =>{
+        let packaged_data = {}
+        let {recipe} = this.state
+
+        let [author, src, quick_description, img, chef_notes, url] = [null, null, null, null, null, null]
+        let descriptors = ['total_time', 'prep_time', 'cook_time', 'servings', 'calories']
+        packaged_data['descriptorList'] = []
+
+        if(recipe['author'] !== ''){
+            author=<h3 className='author'>{recipe['author']}</h3>
+        }
+        packaged_data['author']=author
+
+        if(recipe['src'] !== ''){
+            src=<h4 className='src'>{recipe['src']}</h4>
+        }
+        packaged_data['src']=src
+
+        if(recipe['quick_description'] !== ''){
+            quick_description=<p className='quick_description'>{recipe['quick_description']}</p>
+        }
+        packaged_data['quick_description']=quick_description
+        
+        if(recipe['img'] !== ''){
+            img=<img src={recipe['img']} alt={this.state.title}></img>            
+        }
+        packaged_data['img']=img
+
+        if(recipe.chef_notes !== ''){
+            chef_notes= <p className='chef_notes'>{recipe.chef_notes}</p>
+        }
+        packaged_data['chef_notes']=chef_notes
+        
+        for(var i = 0; i < descriptors.length; i++){
+            let name = descriptors[i]
+            if (recipe[name] !== ''){
+                let temp = <p className='descriptor'>{name}: {recipe[name]}</p>
+                packaged_data.descriptorList.push(temp)
+            }
+        }
+        if(recipe.url !== ''){
+            //rel="noopener noreferrer" is used with target= '_blank' to keep the new window from gaining access
+            //to my index.http target='_blank' opens the link in a new page for more info on this protection and 
+            //risk https://mathiasbynens.github.io/rel-noopener/
+            url = <a href={recipe['url']} target='_blank' rel="noopener noreferrer">{recipe['url']}</a>
+        }
+        packaged_data['url'] = url
+        
+        
+        return packaged_data
+    }
+
+    render(){
+        let {recipe} = this.state
+        if(recipe != null){
+            let packaged_data = this.checkRecipe()
+            return(
+                <div>
+                    <p>in recipe</p>
+                    <h1 className='title'>{recipe.title}</h1>
+                    {packaged_data.author}
+                    {packaged_data.src}
+                    {packaged_data.quick_description}
+                    {packaged_data.img}
+                    {packaged_data.chef_notes}
+                    {packaged_data.descriptorList.map((item,index) => {
+                        return(
+                            <div key={index}>
+                                {item}
+                            </div>
+                        )
+                    })}
+                    <ul className='ingredients-list'>
+                        {recipe.ingredients.map((item,index) =>{
+                            return(
+                                <li key={index}><p className='ingredient-item'>{item}</p></li>
+                            )
+                        })}
+                    </ul>
+                    <p className='directions'>{recipe.directions}</p>
+
+                    {packaged_data.url}
+                    
+
+                </div>
+            )
+        }
+        else{
+            return(
+                <p>Loading Recipe Please wait</p>
+            )
+        }
+    }
+}
+export default DisplayRecipe
