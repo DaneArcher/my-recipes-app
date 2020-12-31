0797d68f4809680acd58bb5f6a40ad630781a764
DaneArcher <danearcher1@gmail.com>
2020-12-31 14:10:53 -0800

    added div classNames to both forms for styling

--- my-react-app/src/components/AddRecipe_Folder/RecipeForm.js
+++ my-react-app/src/components/AddRecipe_Folder/RecipeForm.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import DisplayIngredients from './display_ingredients'
 import AddIngredient from './add_ingredients'
+import './RecipeForm.css'
 
 
 export const RecipeForm = ({recipe, handleChange, addIngredient, deleteIngredient}) =>{
     return(
         <>
-            <label>Title</label>
-            <input name='title' value={recipe.title} onChange={handleChange} placeholder='Title'/>
-            <label>Author</label>
-            <input name='author' value={recipe.author} onChange={handleChange} placeholder='Author'/>
-            <label>Source</label>
-            <input name='src' value={recipe.src} onChange={handleChange} placeholder='Website, Book, Magazine, (optional)'/>
-            <label>Quick Descriptions</label>
-            <textarea name='quick_description' value={recipe.quick_description} onChange={handleChange} placeholder='Toasted tortillas topped with flavorful black beans and fresh vegetables make a quick and delicious...'/>
+            <div className='form-title'>
+                <label>Title</label>
+                <input name='title' value={recipe.title} onChange={handleChange} placeholder='Title'/>
+            </div>
+            <div className='form-author'>
+                <label>Author</label>
+                <input name='author' value={recipe.author} onChange={handleChange} placeholder='Author'/>
+            </div>
+            <div className='form-source'>
+                <label>Source</label>
+                <input name='src' value={recipe.src} onChange={handleChange} placeholder='Website, Book, Magazine, (optional)'/>
+            </div>
+            <div className='form-rating'>
+                <label>Rating</label>
+                <input name='rating' value={recipe.rating} onChange={handleChange} placeholder='4/5'/>
+            </div>
+            <div className='form-Q-description'>
+                <label>Quick Descriptions</label>
+                <textarea name='quick_description' value={recipe.quick_description} onChange={handleChange} placeholder='Toasted tortillas topped with flavorful black beans and fresh vegetables make a quick and delicious...'/>
+            </div>
             <div className='descriptors'>
                 <label>Total Time</label>
                 <input name='total_time' value={recipe.total_time} onChange={handleChange} placeholder='X Hours or X Minutes'/>
@@ -26,19 +39,29 @@ export const RecipeForm = ({recipe, handleChange, addIngredient, deleteIngredien
                 <label>Calories Per Serving</label>
                 <input name='calories' value={recipe.calories} onChange={handleChange} placeholder='375'/>
             </div>
-            <label>Rating</label>
-            <input name='rating' value={recipe.rating} onChange={handleChange} placeholder='4/5'/>
-            <DisplayIngredients ingredients={recipe.ingredients} deleteIngredient={deleteIngredient}/>
-            <AddIngredient addIngredient={addIngredient}/>  
-            <label>Directions</label>
-            <textarea name='directions' value={recipe.directions} onChange={handleChange} placeholder='Start by...'/>
-            <label>Notes</label>
-            <textarea name='chef_notes' value={recipe.chef_notes} onChange={handleChange} placeholder='Simmer for 10 minutes instead of 5 iminutes'/>
-
-            <label>Website URL</label>
-            <input name='url' value={recipe.url} onChange={handleChange} placeholder='www.foodnetwork.com'/>
-            <label>Image Link</label>
-            <input name='img_link' value={recipe.img_link} onChange={handleChange}/>
+            <div className='form-instructions'>
+                <div className='form-ingredients'>
+                    <h4>INGREDIENTS</h4>
+                    <AddIngredient addIngredient={addIngredient}/>
+                    <DisplayIngredients ingredients={recipe.ingredients} deleteIngredient={deleteIngredient}/>
+                </div>  
+                <div className='form-directions'>
+                    <h4>DIRECTIONS</h4>
+                    <textarea name='directions' value={recipe.directions} onChange={handleChange} placeholder='Start by...'/>
+                </div>
+            </div>
+            <div className='form-notes'>
+                <label>Notes</label>
+                <textarea name='chef_notes' value={recipe.chef_notes} onChange={handleChange} placeholder='Simmer for 10 minutes instead of 5 iminutes'/>
+            </div>
+            <div className='form-url'>
+                <label>Website URL</label>
+                <input name='url' value={recipe.url} onChange={handleChange} placeholder='www.foodnetwork.com'/>
+            </div>
+            <div className='form-img'>
+                <label>Image Link</label>
+                <input name='img_link' value={recipe.img_link} onChange={handleChange}/>
+            </div>
         </>
     )
 }
@@ -47,7 +70,7 @@ export const TagForm = ({ingredients, ingredient_tags, parsedIngr}) =>{
     return(
         <>
             {ingredients.map((item, index) =>(
-                <div key={index}>
+                <div className='tagform'key={index}>
                     <label>{item}</label>
                     <input value={ingredient_tags[index]} onChange={(e) => {parsedIngr(e,index)}}/>
                 </div>
